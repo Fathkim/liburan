ff0957331cf7295f6462d809e11d6c27417dbbf7
fathkim <fathanhakim2006@gmail.com>
2022-01-19 11:24:56 +0700

    blockscop

---
+++ hari pertama/test.js
@@ -0,0 +1,17 @@
+// Dasar syntax dari javascript
+
+
+/**
+ * komentar yang lebih dari 1 baris 
+ * w jdcw
+ * uwbdc
+ */
+
+function hello() {
+    if(true){
+        let manggil = "woi manggil nih"
+        console.log(manggil)
+    }
+    console.log(manggil)
+}
+hello()
