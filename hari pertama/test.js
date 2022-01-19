1044ae6eef35a327c6e569842efda2700cc2e680
fathkim <fathanhakim2006@gmail.com>
2022-01-19 15:02:19 +0700

    scope

--- hari pertama/test.js
+++ hari pertama/test.js
@@ -7,11 +7,41 @@
  * uwbdc
  */
 
-function hello() {
+//function hello() {
+   // if(true){
+        //let manggil = "woi manggil nih"
+       // console.log(manggil)
+    //}
+    //console.log(manggil)
+//}
+//hello()
+
+// closure
+
+// function hello(nama, kelas){
+//     console.log("nama :", nama, "\nkelas", kelas)
+// } 
+
+// hello("Muhammad Fathan Hakim", "10 RPL B")
+
+// Azzam IDN, [19 Jan 2022 14.57.31]:
+// let riyad = 'azzam'
+// function ngenal() {
+//     console.log(riyad)
+//     if(true){
+//         const santri = 'rabbani'
+//         console.log(santri)
+//     }
+// }
+// ngenal()
+
+
+let dqa = 'Fathan'
+function ngenal() {
+    console.log(dqa)
     if(true){
-        let manggil = "woi manggil nih"
-        console.log(manggil)
+        const santri = 'hakim'
+        console.log(santri)
     }
-    console.log(manggil)
 }
-hello()
+ngenal()
